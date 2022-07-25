0a0f969cd0fc6883769e51121c36d7ed37f062e4
div1010 <pandeydivyanshu1000@gmail.com>
2022-07-25 11:58:08 +0000

    up_16

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -21,9 +21,15 @@ async def test_seq_bug1(dut):
     dut.reset.value = 1
     await FallingEdge(dut.clk)  
     dut.reset.value = 0
+    
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
     await FallingEdge(dut.clk)
     dut.inp_bit.value=1
     await FallingEdge(dut.clk)
     dut.inp_bit.value=1
+
 
     assert dut.seq_seen.value==1,f"Incorrect output {dut.seq_seen.value} !=1"
