5bec2fcab31999a04975a1f0be79e281c0f8d0fa
div1010 <pandeydivyanshu1000@gmail.com>
2022-07-25 09:30:18 +0000

    update_2

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -7,4 +7,15 @@ from cocotb.triggers import Timer
 async def test_mux(dut):
     """Test for mux2"""
 
-    cocotb.log.info('##### CTB: Develop your test here ########')
+    #cocotb.log.info('##### CTB: Develop your test here ########')
+    SEL=13
+    INP12=3
+    INP13=2
+
+    dut.sel.value=SEL
+    dut.inp12.value=INP12
+    dut.inp13.value=INP13
+
+    await Timer(2,units='ns')
+
+    assert dut.out.value == INP13, f"Multiplexer result incorrect :{dut.out.value}!=2"
