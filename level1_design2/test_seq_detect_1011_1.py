eff869325126db63707d5f2ce74d5b198d4e170c
div1010 <pandeydivyanshu1000@gmail.com>
2022-07-25 16:07:25 +0000

    upd

---
+++ level1_design2/test_seq_detect_1011_1.py
@@ -0,0 +1,42 @@
+# See LICENSE.vyoma for details
+
+# SPDX-License-Identifier: CC0-1.0
+
+import os
+import random
+from pathlib import Path
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge
+
+@cocotb.test()
+async def test_seq_bug1(dut):
+    """Test for seq detection """
+
+    clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
+    cocotb.start_soon(clock.start())        # Start the clock
+
+    # reset
+    dut.reset.value = 1
+    await FallingEdge(dut.clk)  
+    dut.reset.value = 0
+    
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=0
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+
+    dut.inp_bit.value=1
+    await FallingEdge(dut.clk)
+
+
+    
+
+    assert dut.seq_seen.value==1,f"Incorrect output {dut.seq_seen.value} !=1"
