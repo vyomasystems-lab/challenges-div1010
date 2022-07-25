bc4125db0a4653b1dedb17a07cb9e8e80c3dce57
div1010 <pandeydivyanshu1000@gmail.com>
2022-07-25 09:47:04 +0000

    up_6

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,6 +8,7 @@ async def test_mux(dut):
     """Test for mux2"""
 
     #cocotb.log.info('##### CTB: Develop your test here ########')
+    #1st bug:
     SEL=13
     INP12=3
     INP13=2
@@ -19,3 +20,17 @@ async def test_mux(dut):
     await Timer(2,units='ns')
 
     assert dut.out.value == INP13, f"Multiplexer result incorrect :{dut.out.value}!=10"
+
+async def test_mux_1(dut):
+    SEL=30
+    INP30=1
+    #2nd bug:
+
+    dut.sel.value=SEL
+    dut.inp30.value=INP30
+
+    await Timer(2,units='ns')
+
+    assert dut.out.value == INP30, f"Multiplexer result incorrect :{dut.out.value}!=01"
+
+
